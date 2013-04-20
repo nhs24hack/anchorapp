999e24ef33c3559e1b50269fb19974daeef2519c
tehsquidge <liam.d.kerr@gmail.com>
2013-04-20 16:05:09 +0100

    adding template and working page transistions

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,3 +1,10 @@
 function onDeviceReady(){
 	navigator.notification.alert("PhoneGap is working!!");
+	AnchorMap.init('map_canvas');
 }
+
+$(document).ready( function(){
+	$(document).on( "pagechange", function( toPage, object ) {
+		console.log(toPage);
+	});
+});
