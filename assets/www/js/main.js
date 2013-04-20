de2c551f021cb69250db33c0691158b5ebe97807
tehsquidge <liam.d.kerr@gmail.com>
2013-04-20 23:38:38 +0100

    new way to do per page javascript

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,3 +1,12 @@
 function onDeviceReady(){
 
 }
+$(document).ready( function(){
+	$(document).on( 'pagechange', function(obj){
+		var page = $.mobile.activePage.attr('data-url').match(/.*\/(.*)$/)[1];
+		//per page javascript, you may want to break these out to functions
+		if(page == "dropAnchor.html"){
+		    var map = AnchorMap.init('drop-anchor-map');
+		}
+	});
+});
