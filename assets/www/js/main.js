f096ef39973668f470131a268c836a0d730ea3ea
Chae Gribben <chaegribben@hotmail.com>
2013-04-21 04:33:10 +0100

    Saving of anchors Changes to map code

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,12 +1,79 @@
 function onDeviceReady(){
 
 }
+
+function Anchor() {
+	this.timestamp = 0;
+	this.latitude = 0;
+	this.longitude = 0;
+	this.tell = "";
+	this.gesture = "";
+	this.trigger = "";
+	this.attack_rating = 0;
+	this.anchor_rating = 0;
+	this.image = {
+			type : "",
+			src : ""
+		};
+}
+
+function placeAnchor() {
+	var anchor = new Anchor();
+	var map = AnchorMap.map;
+	
+	anchor.timestamp = new Date().getTime();
+	
+	var position = map.getCenter();
+	anchor.latitude = position.lat();
+	anchor.longitude = position.lng();
+	
+	anchor.tell = $('#anchor-tell').val();
+	anchor.gesture = $('#anchor-gesture').val();
+	anchor.trigger = $('#anchor-gesture').val();
+	
+	anchor.attack_rating = $('#anchor-attack-severity').val();
+	anchor.anchor_rating = $('#anchor-rating').val();
+	
+	var storage = window.localStorage;
+	var anchors = JSON.parse(storage.getItem('anchors'));
+	if(anchors == null || !jQuery.isArray(anchors)) {
+		anchors = [];
+	}
+	anchors[anchors.length] = anchor;
+	storage.setItem('anchors', JSON.stringify(anchors));
+	
+	return false;
+}
+
 $(document).ready( function(){
 	$(document).on( 'pagechange', function(obj){
 		var page = $.mobile.activePage.attr('data-url').match(/.*\/(.*)$/)[1];
 		//per page javascript, you may want to break these out to functions
 		if(page == "dropAnchor.html"){
-		    var map = AnchorMap.init('drop-anchor-map');
+		    
+			var map = AnchorMap.init('drop-anchor-map');
+		    
+		    
+		    
+		} else if(page == "viewAnchorsMap.html") {
+			var map = AnchorMap.init('map_canvas');
+			
+			var storage = window.localStorage;
+	    	var anchors = JSON.parse(storage.getItem('anchors'));
+	    	if(anchors != null) {
+	    		var pos;
+				for(var a = 0; a < anchors.length; a++) {
+					var anchor = anchors[a];
+					pos = new google.maps.LatLng(anchor.latitude, anchor.longitude);
+					var marker = new google.maps.Marker({
+						position : pos,
+						map : map,
+						title : ""+anchor.timestamp
+					});
+				}
+				map.setCenter(pos);
+	    	}
+		
 		}
 	});
 });
--- assets/www/js/map.js
+++ assets/www/js/map.js
@@ -1,16 +1,7 @@
 AnchorMap = {
-	map : null,
-	
+		map:null,
 	init : function(id) {
-		var myLatLng = null;
-
-//    	if (navigator.geolocation) {
-//        	navigator.geolocation.getCurrentPosition(function showPosition(position) {
-//        		myLatLng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-//        	});
-//        } else {
-        	myLatLng = new google.maps.LatLng(55.86179, - 4.24388);
-//        }
+		var myLatLng = new google.maps.LatLng(55.86179, - 4.24388);
     	
     	var mapOptions = {
 			zoom: 16,
@@ -18,23 +9,22 @@ AnchorMap = {
 			mapTypeId: google.maps.MapTypeId.SATELLITE,
 			disableDefaultUI: true
 		};
-
-		map = new google.maps.Map(document.getElementById(id), mapOptions);
+    	
+    	var map = new google.maps.Map(document.getElementById(id), mapOptions);
 		this.map = map;
 		
-//		jQuery.getJSON('anchors.json', function (data) {
-//			var anchors = data;
-//			for (var key in anchors) {
-//				var anchor = anchors[key];
-//				if (anchor.lat == '' | anchor.lon == '') {
-//					continue;
-//				}
-//				var marker = new google.maps.Marker( {
-//					position : new google.maps.LatLng(anchor.lat, anchor.lon), map : map, title : anchor.name
-//				});
-//			}
-//		})
-		
-		return this.map;
+    	if (navigator.geolocation) {
+        	navigator.geolocation.getCurrentPosition(
+        			function(position) {
+        				map.setCenter(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
+        			},
+        			function(error){
+        				//TODO:Handle error
+        			},
+        			{ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }
+        		);
+        }
+    	
+		return map;
 	}
 };
